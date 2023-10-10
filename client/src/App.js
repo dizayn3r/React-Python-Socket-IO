e143b68f5de6568519b4da32d6db1f48149ca4ca
Shashi Kant <64494203+dizayn3r@users.noreply.github.com>
2023-10-10 13:17:52 +0530

    home and login ui (#2)

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import Register from './pages/Register.js';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import socketClient from "socket.io-client";
+import Register from "./pages/Register.js";
+import Login from "./pages/Login.js";
+import Home from "./pages/Home.js";
+const SERVER_URL = "http://localhost:5000";
 
 function App() {
+  var socket = socketClient(SERVER_URL);
+
+  socket.on("connect", () => {
+    console.log(`I'm connected to ${SERVER_URL}`);
+  });
 
   return (
-    <main>
-      <Register />
-    </main>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
-export default App
+export default App;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,13 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
-import {
-  faCheck,
-  faTimes,
-  faInfoCircle,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
-const PWD_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+import React from "react";
 
 function Register() {
   return (
@@ -26,8 +17,7 @@ function Register() {
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
         <div class="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
           <form class="space-y-6" action="#" method="POST">
-
-          <div>
+            <div>
               <label
                 for="username"
                 class="block text-sm font-medium leading-6 text-gray-900"
@@ -44,7 +34,7 @@ function Register() {
                 />
               </div>
             </div>
-            
+
             <div>
               <label
                 for="email"
@@ -99,38 +89,12 @@ function Register() {
               </div>
             </div>
 
-            <div class="flex items-center justify-between">
-              <div class="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                />
-                <label
-                  for="remember-me"
-                  class="ml-3 block text-sm leading-6 text-gray-900"
-                >
-                  Remember me
-                </label>
-              </div>
-
-              <div class="text-sm leading-6">
-                <a
-                  href="/#"
-                  class="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-
             <div>
               <button
                 type="submit"
                 class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Sign in
+                Sign Up
               </button>
             </div>
           </form>
@@ -138,7 +102,7 @@ function Register() {
           <p class="mt-10 text-center text-sm text-gray-500">
             Already have an account?
             <a
-              href="/#"
+              href="/login"
               class="ml-2 font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Login
